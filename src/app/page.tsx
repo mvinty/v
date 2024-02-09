728f861cf25a088f8c399af3f67a6bf7155519c4
vinht <vinht@umich.edu>
2024-02-09 14:45:07 -0500

    init2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-pink-400 grid grid-cols-3 gap-2 grid-rows-3"> 
       <div className="flex justify-center items-center col-span-3  row-start-1 row-end-2">
-        <h1 className="text-white text-4xl  ">Will you be my pookie this Valentine's?</h1>
+        <h1 className="text-white text-4xl  ">{`Will you be my pookie this Valentine's?`}</h1>
       </div>
       <div className="flex justify-center  col-start-2 cold-end-3  row-start-2 row-end-3">
         <div className="text-white text-4xl mt-[-150px]">
